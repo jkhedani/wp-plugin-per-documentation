a89738ba3edf84961a57888a1b69451250877389
Justin Hedani <jkhedani@gmail.com>
2014-09-18 21:57:03 -1000

    Setting up class framework.

--- codex.php
+++ codex.php
@@ -4,9 +4,41 @@
  * Plugin URI: https://github.com/jkhedani/wp-plugin-codex
  * Description: A WordPress Plugin aimed at making documentation easier!
  * Version: 0.1.0
+ * Text Domain: codex
  * Author: Justin Hedani
  * Author URI: https://github.com/jkhedani
  * License: MIT
  */
+
+if ( ! class_exists('') ) :
+
+class codex {
+
+  function __construct() {
+
+  }
+
+  /**
+   * Work, work.
+   * Add styles, scripts, custom post types and menu items.
+   *
+   * @since 0.1.0
+   */
+  function codex_init() {
+    // Styles
+    // Functions
+    // Admin stuff
+    if ( is_user_logged_in() && current_user_can('edit_posts') ) {
+
+    }
+  }
+
+  function codex_activate() {
+    // Create documentation post type
+  }
+
+}
+
+endif; // check if class exists
 
 ?>
