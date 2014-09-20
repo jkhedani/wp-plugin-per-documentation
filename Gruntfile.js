e7867d1100718d9e1f16aa21dd1fdd3608aa37db
Justin Hedani <jkhedani@gmail.com>
2014-09-20 00:26:43 -1000

    Update grunt workflow.

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,12 +6,14 @@
  * @link http://gruntjs.com/configuring-tasks#building-the-files-object-dynamically
  * @todo - ensure sourcemaps are working
  * @todo - configure jshint properly
- * @todo - resolve minification issues
  */
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    /** @link https://github.com/gruntjs/grunt-contrib-less */
+    /**
+     * Compile all less files into css
+     * @link https://github.com/gruntjs/grunt-contrib-less
+     */
     less: {
       dist: {
         options: {
@@ -27,44 +29,58 @@ module.exports = function(grunt) {
         }]
       } // development
     },
-    /** @link https://github.com/gruntjs/grunt-contrib-uglify */
+
+    /**
+     * Concat all js files and place is js/
+     * @link https://github.com/gruntjs/grunt-contrib-concat
+     */
+    concat: {
+      options: {
+        separator: ';',
+        banner: '/*! concat <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      prod: {
+        src: ['assets/js/src/*.js'], // the files to concatenate
+        dest: 'assets/js/<%= pkg.name %>.js' // the location of the resulting JS file
+      }
+    },
+
+    /**
+     * Uglify only the concatenated files
+     * @link https://github.com/gruntjs/grunt-contrib-uglify
+     */
     uglify: {
-      dist: {
-        options: {
-          sourceMap: true,
-          sourceMapName: 'assets/js/maps/sourcemap.map',
-          banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-        }
+      options: {
+        sourceMap: true,
+        sourceMapName: 'assets/js/maps/sourcemap.map',
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      prod: {
         files: [{
           expand: true,
-          cwd:    'assets/js/', // From
-          src:    ['**/*.js'],
+          cwd:    'assets/js/', // src directory
+          src:   ['*.js', '!*.min.js'], // src files relative to cwd
           dest:   'assets/js/', // To
           ext:    '.min.js',
           extDot: 'first',
         }]
       }
     },
-    /** @link https://github.com/gruntjs/grunt-contrib-jshint */
-    jshint: {
 
-    },
-    /** @link https://github.com/gruntjs/grunt-contrib-concat */
-    concat: {
-      options: {
-        separator: ';'
-      },
-      dist: {
-        // the files to concatenate
-        src: ['assets/js/**/*.js'],
-        // the location of the resulting JS file
-        dest: 'assets/js/<%= pkg.name %>.js'
-      }
-    },
     /** @link https://github.com/gruntjs/grunt-contrib-watch */
     watch: {
-      files: ['assets/**/*.less'],
-      tasks: ['less']
+      concat: {
+        files: ['assets/js/src/*.js'],
+        tasks: ['concat']
+      },
+      // uglify: {
+      //   files: ['assets/js/*.js'],
+      //   tasks: ['uglify']
+      // },
+      styles: {
+        files: ['assets/**/*.less'],
+        tasks: ['less']
+      }
     }
     // Hook js hint into watch
   });
@@ -72,10 +88,9 @@ module.exports = function(grunt) {
   // Load plugin(s)
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s)
-  grunt.registerTask('default',['watch','uglify']);
+  grunt.registerTask('default',['watch']);
 };
